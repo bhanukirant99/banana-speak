87fd27686c0f698e1a8c19892ae1a4551370ccba
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-18 05:18:23 +0530

    feat: adding style to output

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textAreaValue = document.getElementById("text-area-value");
-var translatedOutput = document.getElementById("translated-output")
+var translateOutput = document.getElementById("translate-output")
 
 
 function takeInput () {
     console.log("textAreaValue", textAreaValue.value)
-    translatedOutput.innerHTML = textAreaValue.value
+    translateOutput.innerHTML = textAreaValue.value
 }
 
 btnTranslate.addEventListener("click", takeInput)
