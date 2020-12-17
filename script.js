ed32effd25fac7a4c59693d652c779534acc754d
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-18 05:12:29 +0530

    feat: showing output in div

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textAreaValue = document.getElementById("text-area-value");
+var translatedOutput = document.getElementById("translated-output")
+
 
 function takeInput () {
-    console.log("textAreaValue", textArea.value)
+    console.log("textAreaValue", textAreaValue.value)
+    translatedOutput.innerHTML = textAreaValue.value
 }
 
 btnTranslate.addEventListener("click", takeInput)
