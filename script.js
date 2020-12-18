6e65fc09ac9d763d54c7497fee7a9ff52da19ea7
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-18 05:49:07 +0530

    feat: fetching data from the url

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ var translateOutput = document.getElementById("translate-output")
 
 function takeInput () {
     console.log("textAreaValue", textAreaValue.value)
+    var url = `https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textAreaValue.value}`;
     translateOutput.innerHTML = textAreaValue.value
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))
 }
 
 btnTranslate.addEventListener("click", takeInput)
